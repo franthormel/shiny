e1c4712bdbfe1642bc1cd5b0fc90a86847fc4f0b
Francis Anthony <fcaboyo@gmail.com>
2024-09-22 13:18:11 +0800

    Add currency formatting

--- ph-imts/app.py
+++ ph-imts/app.py
@@ -1,6 +1,7 @@
+from data import df, df_all, df_yearly, df_month_year
 from shiny.express import input, module, render, ui
 from shinywidgets import render_widget
-from data import df, df_all, df_yearly, df_month_year
+import locale
 import plotly.express as px
 
 ui.page_opts(
@@ -9,41 +10,55 @@ ui.page_opts(
     lang="en",
 )
 
+def initialize():
+    locale.setlocale(locale.LC_ALL, "")
+initialize()
+
+def format_currency(value):
+    output = locale.currency(value, grouping=True)
+    
+    # Formatting a negative value encloses the output string inside parentheses.
+    # Remove the parentheses and put the negative sign instead.
+    if(output.startswith('(') and output.endswith(')')):
+        return f"-{output[1:-1]}"
+    
+    return output
+
 @module
-def cards_summary(input, output, session):
+def cards_summary(input, output, session, df):
     with ui.layout_columns(fill=False):
         with ui.value_box():
             "Exports"
             
             @render.express
             def exports():
-                df_all['exports']
+                format_currency(df['exports'])
 
         with ui.value_box():
             "Imports"
             
             @render.express
             def imports():
-                df_all['imports']
+                format_currency(df['imports'])
 
         with ui.value_box():
             "Balance of Trade"
             
             @render.express
-            def bot():
-                df_all['botg']
+            def botg():
+                format_currency(df['botg'])
                 
         with ui.value_box():
             "Total Trade"
             
             @render.express
             def total():
-                df_all['total_trade']
+                format_currency(df['total_trade'])
 
 with ui.navset_card_pill(id="navset_current"):
     with ui.nav_panel("All"):
         with ui.layout_columns(fill=False, col_widths=[12, 12, 6, 6, 12]):
-            cards_summary("cards_summary_all")
+            cards_summary("cards_summary_all", df_all)
                 
             with ui.card():
                 with ui.card_header():
@@ -66,7 +81,8 @@ with ui.navset_card_pill(id="navset_current"):
                 "Put DataGrid chart here"
                     
     with ui.nav_panel("Yearly"):
-        cards_summary("cards_summary_yearly")
+        cards_summary("cards_summary_yearly", df_all)
         
     with ui.nav_panel("Monthly"):
-        cards_summary("cards_summary_monthly")
+        # TODO: Use correct data
+        cards_summary("cards_summary_monthly", df_all)

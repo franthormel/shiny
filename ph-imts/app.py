9eda95aea4cef90522db43e84d07c447756ab10e
Francis Anthony <fcaboyo@gmail.com>
2024-09-22 12:19:08 +0800

    Add grid layout

--- ph-imts/app.py
+++ ph-imts/app.py
@@ -3,7 +3,11 @@ from shinywidgets import render_widget
 from data import df, df_all, df_yearly, df_month_year
 import plotly.express as px
 
-ui.panel_title("Philippine Total Trade, Imports, Exports, and Balance of Trade in Goods by Month and Year: 1991-2023")
+ui.page_opts(
+    title="Philippine Total Trade, Imports, Exports, and Balance of Trade in Goods by Month and Year: 1991-2023",
+    fillable=True,
+    lang="en",
+)
 
 @module
 def cards_summary(input, output, session):
@@ -36,15 +40,33 @@ def cards_summary(input, output, session):
             def total():
                 df_all['total_trade']
 
-with ui.sidebar(position="right"):
-    "Sidebar"
-    
 with ui.navset_card_pill(id="navset_current"):
     with ui.nav_panel("All"):
-        cards_summary("cards_summary_all")
+        with ui.layout_columns(fill=False, col_widths=[12, 12, 6, 6, 12]):
+            cards_summary("cards_summary_all")
+                
+            with ui.card():
+                with ui.card_header():
+                    "Summary"
+                "Put summary here"
+            
+            with ui.card(full_screen=True):
+                with ui.card_header():
+                    "Timeline"
+                "Put line chart here"
+                
+            with ui.card(full_screen=True):
+                with ui.card_header():
+                    "Trade Composition"
+                "Put pie chart here"
+                
+            with ui.card(full_screen=True):
+                with ui.card_header():
+                    "Data Table"
+                "Put DataGrid chart here"
                     
     with ui.nav_panel("Yearly"):
         cards_summary("cards_summary_yearly")
         
     with ui.nav_panel("Monthly"):
-        "Monthly"
+        cards_summary("cards_summary_monthly")

8893f57367c4f1d29800fb3a724072eeef2b1794
Francis Anthony <fcaboyo@gmail.com>
2024-09-26 12:25:42 +0800

    Add growth rate values and charts

--- ph-imts/app.py
+++ ph-imts/app.py
@@ -1,4 +1,4 @@
-from data import df, df_all, df_year, df_chart_all_import_exports
+from data import df, df_all, df_year, df_chart_all_import_exports, df_chart_all_import_exports_growth_rate
 from shared import initialize, format_currency
 from shiny import reactive
 from shiny.express import input, module, render, ui
@@ -11,11 +11,13 @@ ui.page_opts(
     lang="en",
 )
 
+# Set locale so it can be used for formatting trade values
 initialize()
 
 @module
 def cards_summary(input, output, session, df):
     with ui.layout_columns(fill=False):
+        # Exports
         with ui.value_box():
             "Exports"
             
@@ -23,6 +25,7 @@ def cards_summary(input, output, session, df):
             def exports():
                 format_currency(df['exports'])
 
+        # Imports
         with ui.value_box():
             "Imports"
             
@@ -30,6 +33,7 @@ def cards_summary(input, output, session, df):
             def imports():
                 format_currency(df['imports'])
 
+        # Balance of Trade
         with ui.value_box():
             "Balance of Trade"
             
@@ -37,6 +41,7 @@ def cards_summary(input, output, session, df):
             def balance_of_trade():
                 format_currency(df['balance_of_trade'])
                 
+        # Total TRade
         with ui.value_box():
             "Total Trade"
             
@@ -44,19 +49,35 @@ def cards_summary(input, output, session, df):
             def total():
                 format_currency(df['total_trade'])
 
+def change_plotly_legend_position(fig):
+    fig.update_layout(legend=dict(
+                        yanchor="top",
+                        y=0.97,
+                        xanchor="left",
+                        x=0.01
+                    ))
+
 with ui.navset_card_pill(id="navset_current"):
     with ui.nav_panel("All"):
-        with ui.layout_columns(fill=False, col_widths=[12, 12, 6, 6, 12]):
+        with ui.layout_columns(fill=False, col_widths=[12, 12, 12, 6, 6, 12]):
+            # Summary
             cards_summary("cards_summary_all", df_all)
-                
             
+            # Trade Values (Line charts)
             with ui.card(full_screen=True):
+                chart_labels_values={
+                    "values": "Trade Value (million USD)",
+                    "type": "Type",
+                    "date": "Trade Month & Year",
+                    "total_trade": "Total Trade Value (million USD)",
+                    "balance_of_trade": "Balance of Trade Value (million USD)",
+                }
+                
                 with ui.card_header():
                     "Trade Values"
                 
                 ui.input_checkbox("checkbox_line_markers", "Show markers", False)
                 
-                # TODO: Add charts for growth rates                    
                 @render_widget
                 def line_chart_import_exports():
                     fig = px.line(
@@ -65,34 +86,11 @@ with ui.navset_card_pill(id="navset_current"):
                         y="values",
                         markers=input.checkbox_line_markers(),
                         color='type',
-                        title="Philippines Imports, Exports from 1991 to 2023",
-                        labels={
-                            "values": "Trade Value (million USD)",
-                            "type": "Type",
-                            "date": "Trade Month & Year",
-                        }
+                        title="Imports, Exports from 1991 to 2023",
+                        labels=chart_labels_values
                     )
-                    fig.update_layout(legend=dict(
-                        yanchor="top",
-                        y=0.97,
-                        xanchor="left",
-                        x=0.01
-                    ))
+                    change_plotly_legend_position(fig)
                     return fig
-                
-                @render_widget
-                def line_chart_balance():
-                    return px.line(
-                        df,
-                        x="date",
-                        y="balance_of_trade",
-                        markers=input.checkbox_line_markers(),
-                        title="Philippines Balance of Trade Value from 1991 to 2023",
-                        labels={
-                            "balance_of_trade": "Balance of Trade Value (million USD)",
-                            "date": "Trade Month & Year",
-                        }
-                    )
                 
                 @render_widget
                 def line_chart_total():
@@ -101,27 +99,92 @@ with ui.navset_card_pill(id="navset_current"):
                         x="date",
                         y="total_trade",
                         markers=input.checkbox_line_markers(),
-                        title="Philippines Total Trade Value from 1991 to 2023",
-                        labels={
-                            "total_trade": "Total Trade Value (million USD)",
-                            "date": "Trade Month & Year",
-                        }
+                        title="Total Trade Value from 1991 to 2023",
+                        labels=chart_labels_values
                     )
                 
+                @render_widget
+                def line_chart_balance():
+                    return px.line(
+                        df,
+                        x="date",
+                        y="balance_of_trade",
+                        markers=input.checkbox_line_markers(),
+                        title="Balance of Trade Value from 1991 to 2023",
+                        labels=chart_labels_values
+                    )
+                
+            # Trade Values Growth Rates (Line charts)
+            with ui.card(full_screen=True):
+                chart_labels_growth_rates={
+                    "values": "Trade Value Growth Rates (percentage)",
+                    "type": "Type",
+                    "date": "Trade Month & Year",
+                    "total_trade_growth_rate": "Total Trade Value Growth Rates (percentage)",
+                    "balance_of_trade_growth_rate": "Balance of Trade Value Growth Rates (percentage)",
+                }
+                
+                with ui.card_header():
+                    "Trade Values Growth Rates"
+                
+                ui.input_checkbox("checkbox_line_growth_rates_markers", "Show markers", False)
+                        
+                @render_widget
+                def line_chart_import_exports_growth_rate():
+                    fig = px.line(
+                        df_chart_all_import_exports_growth_rate,
+                        x="date",
+                        y="values",
+                        markers=input.checkbox_line_growth_rates_markers(),
+                        color='type',
+                        title="Imports, Exports Growth Rates from 1991 to 2023",
+                        labels=chart_labels_growth_rates
+                    )
+                    change_plotly_legend_position(fig)
+                    return fig
+                
+                @render_widget
+                def line_chart_total_growth_rate():
+                    return px.line(
+                        df,
+                        x="date",
+                        y="total_trade_growth_rate",
+                        markers=input.checkbox_line_growth_rates_markers(),
+                        title="Total Trade Value Growth Rates from 1991 to 2023",
+                        labels=chart_labels_growth_rates
+                    )
+                
+                @render_widget
+                def line_chart_balance_growth_rate():
+                    return px.line(
+                        df,
+                        x="date",
+                        y="balance_of_trade_growth_rate",
+                        markers=input.checkbox_line_growth_rates_markers(),
+                        title="Balance of Trade Value Growth Rates from 1991 to 2023",
+                        labels=chart_labels_growth_rates
+                    )
+                
+            # TODO: Summary
             with ui.card():
                 with ui.card_header():
                     "Summary"
-                # TODO: Add                    
                 "Put summary here"
                 
+            # Trade Composition (Pie chart)
             with ui.card(full_screen=True):
                 with ui.card_header():
                     "Trade Composition"
                 
                 @render_widget
                 def pie_chart():
-                    return px.pie(data_frame=df_chart_all_import_exports, values='values', names='type')
+                    return px.pie(
+                        data_frame=df_chart_all_import_exports, 
+                        values='values', 
+                        names='type'
+                    )
                 
+            # Trade Data (DataGrid)
             with ui.card(full_screen=True):
                 with ui.card_header():
                     "Trade Data"
--- ph-imts/data.py
+++ ph-imts/data.py
@@ -8,13 +8,16 @@ df.imports = pd.to_numeric(df.imports).astype(float)
 df['balance_of_trade'] = df.apply(lambda row: row.exports - row.imports, axis=1)
 df['total_trade'] = df.apply(lambda row: row.exports + row.imports, axis=1)
 
+def calc_growth_rate(df, col):
+    return df[col].pct_change().apply(lambda row: row * 100)
+
 # NOTE: All data
 df_all = df.copy().set_index('year').sum(numeric_only=True)
 
-df['imports_growth_rate'] = df['imports'].pct_change()
-df['exports_growth_rate'] = df['exports'].pct_change()
-df['balance_of_trade_growth_rate'] = df['balance_of_trade'].pct_change()
-df['total_trade_growth_rate'] = df['total_trade'].pct_change()
+df['imports_growth_rate'] = calc_growth_rate(df, 'imports')
+df['exports_growth_rate'] = calc_growth_rate(df, 'exports')
+df['balance_of_trade_growth_rate'] = calc_growth_rate(df, 'balance_of_trade')
+df['total_trade_growth_rate'] = calc_growth_rate(df, 'total_trade')
 df['date'] = df.apply(lambda row: create_date(row), axis=1)
 
 # NOTE: Yearly data
@@ -28,26 +31,30 @@ df_year = df_year.sum(numeric_only=True)
 
 for i, column_rate in enumerate(column_rates):
     column_rate_src = column_rates_src[i]
-    df_year[column_rate] = df_year[column_rate_src].pct_change()
+    df_year[column_rate] = calc_growth_rate(df_year, column_rate_src)
 
 # NOTE: Monthly data
 # Group by month per year
 df_month_each_year = df.pivot(index="year", columns="month")
 
-def categorize_col_vals(df, col):
+def categorize_col_vals(df, col, type=''):
     # Get column values
     new_df = pd.DataFrame(df[col]).rename(columns={col: "values"})
-    new_df['type'] = col
+    
+    if type == '':
+        type = col
+    
+    new_df['type'] = type
     new_df['date'] = df['date']
     return new_df
 
 # NOTE: Charts (all)
 df_chart_all_import_exports = pd.concat([
-    categorize_col_vals(df, "exports"),
-    categorize_col_vals(df, "imports")
+    categorize_col_vals(df, "exports", "Exports"),
+    categorize_col_vals(df, "imports", "Imports")
 ])
 
 df_chart_all_import_exports_growth_rate = pd.concat([
-    categorize_col_vals(df, "exports_growth_rate"),
-    categorize_col_vals(df, "imports_growth_rate")
+    categorize_col_vals(df, "exports_growth_rate", "Exports"),
+    categorize_col_vals(df, "imports_growth_rate", "Imports")
 ])

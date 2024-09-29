c0e4660424e9dff48c7bcfff964f6bc73b7ea6e2
franthormel <fcaboyo@gmail.com>
2024-09-29 15:07:32 +0800

    Update yearly data

--- ph-imts/data.py
+++ ph-imts/data.py
@@ -11,7 +11,7 @@ df['total_trade'] = df.apply(lambda row: row.exports + row.imports, axis=1)
 def calc_growth_rate(df, col):
     return df[col].pct_change().apply(lambda row: row * 100)
 
-# NOTE: All data
+# NOTE: All
 df_all = df.copy().set_index('year').sum(numeric_only=True)
 
 df['imports_growth_rate'] = calc_growth_rate(df, 'imports')
@@ -20,7 +20,31 @@ df['balance_of_trade_growth_rate'] = calc_growth_rate(df, 'balance_of_trade')
 df['total_trade_growth_rate'] = calc_growth_rate(df, 'total_trade')
 df['date'] = df.apply(lambda row: create_date(row), axis=1)
 
-# NOTE: Yearly data
+def categorize_col_vals(df, col, type=''):
+    # Get column values
+    new_df = pd.DataFrame(df[col]).rename(columns={col: "values"})
+    if type == '':
+        type = col
+    new_df['type'] = type
+    return new_df
+
+# NOTE: All (Charts)
+def categorize_all_col_vals(df, col, type=''):
+    new_df = categorize_col_vals(df, col, type)
+    new_df['date'] = df['date']
+    return new_df
+
+df_all_chart_import_exports = pd.concat([
+    categorize_all_col_vals(df, "exports", "Exports"),
+    categorize_all_col_vals(df, "imports", "Imports")
+])
+
+df_all_chart_import_exports_growth_rate = pd.concat([
+    categorize_all_col_vals(df, "exports_growth_rate", "Exports"),
+    categorize_all_col_vals(df, "imports_growth_rate", "Imports")
+])
+
+# NOTE: Yearly
 column_rates = ["imports_growth_rate", "exports_growth_rate", "balance_of_trade_growth_rate", "total_trade_growth_rate"]
 column_rates_src = ["imports", "exports", "balance_of_trade", "total_trade"]
 
@@ -31,32 +55,18 @@ df_yearly = df_yearly.sum(numeric_only=True)
 for i, column_rate in enumerate(column_rates):
     column_rate_src = column_rates_src[i]
     df_yearly[column_rate] = calc_growth_rate(df_yearly, column_rate_src)
-
-# NOTE: Monthly data
-df_month_each_year = df.pivot(index="year", columns="month")
-
-def categorize_col_vals(df, col, type=''):
-    # Get column values
-    new_df = pd.DataFrame(df[col]).rename(columns={col: "values"})
     
-    if type == '':
-        type = col
-    
-    new_df['type'] = type
-    new_df['date'] = df['date']
-    return new_df
-
-# NOTE: Charts (all)
-df_all_chart_import_exports = pd.concat([
-    categorize_col_vals(df, "exports", "Exports"),
-    categorize_col_vals(df, "imports", "Imports")
+# NOTE: Yearly (Charts)
+df_yearly_chart_import_exports = pd.concat([
+    categorize_col_vals(df_yearly, "exports", "Exports"),
+    categorize_col_vals(df_yearly, "imports", "Imports")
 ])
 
-df_all_chart_import_exports_growth_rate = pd.concat([
-    categorize_col_vals(df, "exports_growth_rate", "Exports"),
-    categorize_col_vals(df, "imports_growth_rate", "Imports")
-])
-
-# NOTE: Datagrid (yearly)
+# NOTE: Yearly (DataGrid)
 df_yearly_indices = pd.DataFrame(df_yearly.index, index=df_yearly.index)
 df_yearly_datagrid = pd.concat([df_yearly_indices, df_yearly], axis=1)
+
+
+# NOTE: Monthly
+df_month_each_year = df.pivot(index="year", columns="month")
+
